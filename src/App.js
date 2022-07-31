0c0c240898c9023da86cf7581533bcadbd070a68
davidyoon <qqpo12@naver.com>
2022-07-31 19:29:26 +0900

    Add before style on pickButton
    * Added style components using before for shadow effects

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux"
 import { PinkButton } from "./components/PinkButton";
 import { next } from "./store/modules/score"
+import styled from "styled-components";
+
+const Header = styled.h1`
+
+`
 
 function App() {
   const page = useSelector(state => state.score.page)
@@ -12,7 +17,7 @@ function App() {
       {page === 0 && (
         <main>
          <img />
-         <h1>나라별 수도 퀴즈</h1>
+         <Header>나라별 수도 퀴즈</Header>
          <h2>진정한 수도 고인물도 100점을 맞기 어렵습니다!</h2>
          <PinkButton text="테스트 시작!" clickEvent={() => {
           dispatch(next())
